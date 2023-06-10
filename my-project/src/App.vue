5c1b2e01fab1d3f6a099c0a036afa573bef26884
asaa <2315812245@qq.com>
2023-06-10 17:54:11 +0800

    1

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">Logo</a>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <router-link className="nav-link" to="/index">首页</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#"></a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/index">首页</router-link>
           </li>
-          <li className="nav-item">
-            <router-link className="nav-link" to="/course">课程</router-link>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/course">课程</router-link>
           </li>
-          <li className="nav-item">
-            <router-link className="nav-link" to="/user">用户</router-link>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/user">用户</router-link>
           </li>
         </ul>
       </div>
     </nav>
     <hr>
-    <div className="container">
+    <div class="container">
       <router-view></router-view>
     </div>
   </div>
@@ -36,7 +36,7 @@ const router = new VueRouter({
 });
 
 export default {
-  name: 'App',
+  name:'App',
   router
 };
 </script>
