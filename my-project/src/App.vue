e8d2af026415c335cccca92c2dfee3785676328c
asaa <2315812245@qq.com>
2023-06-10 16:26:39 +0800

    1

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
+    <bar>
+      <baritem v-for="item in datalist" :path="item.path" :color="item.color" :key="item.path">
+        <img slot="icon" :src="item.image" alt="">
+        <img slot="icon-active" :src="item.image_active" alt="">
+        <span  slot="text">{{item.text}}</span>
+      </baritem>
+    </bar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import bar from './components/bar.vue';
+import baritem from './components/baritem.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  components:{
+    bar,
+    baritem
+  },
+  data(){
+    return {
+      datalist:[
+        {
+          path:'/home',
+          color:'red',
+          image:require('./assets/img/tabbar/首页.png'),
+          image_active:require('./assets/img/tabbar/首页_active.png'),
+          text:'首页'
+        },
+        {
+          path:'/type',
+          color:'red',
+          image:require('./assets/img/tabbar/分类.png'),
+          image_active:require('./assets/img/tabbar/分类_active.png'),
+          text:'分类'
+        },
+        {
+          path:'/cart',
+          color:'red',
+          image:require('./assets/img/tabbar/购物车.png'),
+          image_active:require('./assets/img/tabbar/购物车_active.png'),
+          text:'购物车'
+        },
+        {
+          path:'/profile',
+          color:'red',
+          image:require('./assets/img/tabbar/我 的.png'),
+          image_active:require('./assets/img/tabbar/我的_active.png'),
+          text:'我的'
+        }
+
+      ]
+    }
+
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,19 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
-
-
-
-// 导入组件
-
-
-// 定义路由
-
-
-// 创建路由实例
-
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
