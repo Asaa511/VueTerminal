e33cfdd8294f9846e3a978822ca76b6d7c2165a1
3520922005@qq.com <3520922005@qq.com>
2023-06-10 17:19:14 +0800

    hhh

---
+++ my-project/src/Myindex.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>首页
+    <div>首页</div>
+    <div>首页</div>
+    <div>首页</div>
+    <div>首页</div>
+    <div>首页</div></div>
+</template>
+<script>
+    export default{
+        name:"MyIndex"
+    }
+</script>
