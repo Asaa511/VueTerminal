096d5faeed1ead47635d00834f69280ebcbd3912
asaa <2315812245@qq.com>
2023-06-10 17:05:05 +0800

    `

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,46 +1,28 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Logo</a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/index">首页</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/course">课程</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/user">用户</router-link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <hr>
-    <div class="container">
-      <router-view></router-view>
-    </div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import Vue from "vue";
-import VueRouter from "vue-router";
-import routes from './routes/index';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-
-Vue.use(VueRouter);
-const router = new VueRouter({
-  routes
-});
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name:'App',
-  router
-};
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,16 +1,5 @@
-import Vue from 'vue';
-import App from './App.vue';
-
-
-
-// 导入组件
-
-
-// 定义路由
-
-
-// 创建路由实例
-
+import Vue from 'vue'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
