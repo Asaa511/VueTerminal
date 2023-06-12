1f3700b0bd8a4f454dc1dfd4db8a7300b493fc68
asaa <2315812245@qq.com>
2023-06-12 13:47:13 +0800

    13.19

--- my-project/src/components/MyMsg.vue
+++ my-project/src/components/MyMsg.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <p>2</p>
-    <ul>
-      <li v-for="movie in movies" :key="movie.id">
-        {{movie.id}} - {{ movie.title }} - {{ movie.msg }} - {{movie.times}}
+    <div class="mb-3">
+      <input class="form-control" id="titleInput" v-model="newMovie.title" placeholder="电影名称">
+    </div>
+    <div class="mb-3">
+      <input class="form-control" id="msgInput" v-model="newMovie.msg" placeholder="分享的你对这部电影的见解">
+    </div>
+    <button class="btn btn-primary" @click="sendData">发送</button>
+    <ul class="list-group mt-3">
+      <li v-for="movie in movies" :key="movie.id" class="list-group-item">
+        {{ movie.id }} - {{ movie.title }} - {{ movie.msg }} - {{ movie.times }}
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-// eslint-disable-next-line no-unused-vars
 import axios from 'axios';
 import io from 'socket.io-client';
 
@@ -18,27 +23,37 @@ export default {
   data() {
     return {
       movies: [],
+      newMovie: {
+        title: '',
+        msg: '',
+      },
     };
   },
   created() {
-    // 创建Socket.io连接
     const socket = io('http://localhost:3000');
 
-    // 监听服务器发送的新电影数据
     socket.on('newMovie', (newMovie) => {
       this.movies.push(newMovie);
     });
 
-    // 请求初始化电影数据
     this.fetchMovies();
   },
   methods: {
     fetchMovies() {
-      // 发起HTTP GET请求获取电影数据
       axios
           .get('http://localhost:3000')
           .then((response) => {
             this.movies = response.data;
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+    },
+    sendData() {
+      axios
+          .post('http://localhost:3000/addMovies', this.newMovie)
+          .then((response) => {
+            console.log(response.data);
           })
           .catch((error) => {
             console.error(error);
