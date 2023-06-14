44897bb303887ebb3e6a92d89a25a02151af5581
ldb <2117704289@qq.com>
2023-06-14 16:30:14 +0800

    11

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -15,7 +15,14 @@ import MyNavbar from './components/MyNavbar.vue';
 import MyIndex from "@/components/MyIndex.vue";
 import MyUser from "@/components/MyUser.vue";
 import MyMovie from "@/components/MyMovie.vue";
-
+import  BxJg from "@/components/movie/BxJg.vue";
+import CmDsj from "@/components/movie/CmDsj.vue";
+import JsZs from "@/components/movie/JsZs.vue";
+import SdyJq from "@/components/movie/SdyJq.vue";
+import SsDtl from "@/components/movie/SsDtl.vue";
+import XJ from "@/components/movie/XJ.vue";
+import XmHyj from "@/components/movie/XmHyj.vue";
+import AgZz from "@/components/movie/AgZz.vue";
 
 Vue.use(VueRouter);
 const router = new VueRouter({
@@ -34,6 +41,22 @@ export default {
     // eslint-disable-next-line vue/no-unused-components
     MyMovie,
      // eslint-disable-next-line vue/no-unused-components
+    BxJg,
+      // eslint-disable-next-line vue/no-unused-components
+    CmDsj,
+      // eslint-disable-next-line vue/no-unused-components
+    JsZs,
+      // eslint-disable-next-line vue/no-unused-components
+    SdyJq,
+      // eslint-disable-next-line vue/no-unused-components
+    SsDtl,
+      // eslint-disable-next-line vue/no-unused-components
+      XJ,
+      // eslint-disable-next-line vue/no-unused-components
+      XmHyj,
+      // eslint-disable-next-line vue/no-unused-components
+      AgZz,
+      // eslint-disable-next-line vue/no-unused-components
   }
 };
 </script>
